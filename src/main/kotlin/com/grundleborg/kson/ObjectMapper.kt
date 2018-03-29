a92394262706f87b81d099b1fcc4b9164132d54c
George Goldberg <george@gberg.me>
2018-03-29 17:34:43 +0100

    Add license headers to source and some trivial cleanup.

--- src/main/kotlin/com/grundleborg/kson/ObjectMapper.kt
+++ src/main/kotlin/com/grundleborg/kson/ObjectMapper.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 George Goldberg.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.grundleborg.kson
 
 import java.io.Reader
@@ -5,9 +21,7 @@ import kotlin.reflect.KClass
 import kotlin.reflect.KParameter
 import kotlin.reflect.full.primaryConstructor
 import kotlin.reflect.jvm.jvmErasure
-import com.sun.xml.internal.ws.streaming.XMLStreamReaderUtil.close
 import java.lang.reflect.Type
-
 
 class ObjectMapper {
     val typeMap: MutableMap<Type, KClass<*>> = mutableMapOf()
